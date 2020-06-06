f297e02b44877cec902946af14df8b68048a0948
Fatih Baytar <matanist@hotmail.com>
2020-06-06 23:09:26 +0300

    Initial check-in of module BilmemYol

---
+++ BilmemYol.Web/Contollers/HomeController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BilmemYol.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace BilmemYol.Web.Contollers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var client = new RestClient("http://localhost:5000/api/product/getproducts");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            if (response.IsSuccessful)
+            {
+                var content = response.Content;
+                var apiResponse= JsonConvert.DeserializeObject<ApiResponse>(content);
+                if (apiResponse.Code == 200)
+                {
+                    var products = JsonConvert.DeserializeObject<List<Product>>(apiResponse.Set.ToString());
+                    return View(products);
+                }
+
+            }
+            return View();
+        }
+    }
+}
---
+++ BilmemYol.Web/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace BilmemYol.Web.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public string Brand { get; set; }
+        public byte[] Image { get; set; }
+        public int? CategoryId { get; set; }
+
+    }
+}
